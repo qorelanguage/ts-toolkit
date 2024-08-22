f8a5bd09c3954f244dfeaafb42054bed282785c5
Foxhoundn <filip.witosz+textace@gmail.com>
2024-08-22 15:39:37 +0200

    New qorus typings for options (now forms)

---
+++ src/types/qorus.ts
@@ -0,0 +1,21 @@
+export type TQorusInterfaces = 'mapper' | 'workflow' | 'service' | 'job' | 'connection';
+
+/* Types used in UIs, these are not 1:1 to Qore types */
+export type TQorusStringCompatibleUIType = 'binary' | 'date' | 'url' | 'email' | 'string' | 'file-as-string';
+export type TQorusNumberCompatibleUIType = 'int' | 'integer' | 'float' | 'number';
+export type TQorusListCompatibleUIType = 'list' | 'range';
+export type TQorusHashCompatibleUIType = 'hash' | 'data' | 'rgbcolor';
+export type TQorusNullCompatibleUIType = 'null' | 'nothing';
+export type TQorusAnyCompatibleUIType = 'any' | 'auto';
+export type TQorusBooleanCompatibleUIType = 'bool' | 'boolean';
+export type TQorusSpecialUIType = 'richtext' | 'data-provider';
+
+export type TQorusType =
+  | TQorusStringCompatibleUIType
+  | TQorusNumberCompatibleUIType
+  | TQorusListCompatibleUIType
+  | TQorusHashCompatibleUIType
+  | TQorusNullCompatibleUIType
+  | TQorusAnyCompatibleUIType
+  | TQorusBooleanCompatibleUIType
+  | TQorusSpecialUIType;
